13e63d913d210f06983b737b5e97b73ddf7a2f96
Daniela <daraujoz@unal.edu.co>
2021-10-13 10:31:54 -0500

    Primera versión

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy"
 
-console.log('Happy hacking :)')
+//Creación de los nodos para las imagenes a visualizar en el navegador.
+const min = 1
+const max = 122
+const random = () => Math.floor(Math.random() * (max-min)) +min
+
+const createImageNode = ()=>{
+    const container = document.createElement("div")
+    container.className = "p-5"
+
+    const image = document.createElement("img")
+    image.id="img"
+    image.className = "mx-auto"
+    image.width = "380"
+    image.dataset.src =`https://randomfox.ca/images/${random()}.jpg` //dataset comunica entre HTML y JS.
+
+    container.appendChild(image)
+    return container
+}
+const newImage = createImageNode()
+const mountNode = document.getElementById("images")
+const addButton = document.getElementById("add")
+const removeButton = document.getElementById("remove")
+const removeOneImageButton = document.getElementById("addOne")
+
+//Funciones con los botones
+const add = () => {
+    const newImage = createImageNode()
+    mountNode.append(newImage)
+    registerImage(newImage)
+}
+const removeOneImage = ()=>{
+    const hijo = document.querySelector(".p-5")
+    mountNode.removeChild(hijo)
+}
+
+const remove = () => {//Para limpiar la pantala
+    mountNode.innerHTML = ""
+}
+
+//Eventos
+addButton.addEventListener("click", add)
+removeOneImageButton.addEventListener("click", removeOneImage)
+removeButton.addEventListener("click", remove)
